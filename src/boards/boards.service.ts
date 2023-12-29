64225e0a872db9625f795f4838db48c98fa9416c
gull2365 <jych2365@gmail.com>
2023-12-29 22:23:55 +0900

    feat: Board model 정의

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board.model';
 
 @Injectable()
 export class BoardsService {
-  private boards = [];
+  private boards: Board[] = [];
 
-  getAllBoards() {
-    return this.boards;
+  getAllBoards(): Board[] {
+    return this.boards; //return 값이 Board[]여서 타입지정.
   }
 }
