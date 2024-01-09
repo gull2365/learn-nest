d9c62312c4b3aaba47e39cd4c289f48ea001d298
gull2365 <jych2365@gmail.com>
2024-01-09 19:18:38 +0900

    feat: 인증 기능 구현을 위한 준비

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+import { DataSource, Repository } from 'typeorm';
+import { User } from './user.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+  constructor(dataSource: DataSource) {
+    super(User, dataSource.createEntityManager());
+  }
+}
