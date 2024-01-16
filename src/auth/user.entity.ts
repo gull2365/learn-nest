35cf010994ecb92e0afaaa0d192142185331b812
gull2365 <jych2365@gmail.com>
2024-01-17 00:21:38 +0900

    feat: 유저와 게시물의 관계 형성 해주기

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,8 +1,10 @@
 /*eslint-disable */
+import { Board } from 'src/boards/board.entity';
 import {
   BaseEntity,
   Column,
   Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
   Unique,
 } from 'typeorm';
@@ -18,4 +20,7 @@ export class User extends BaseEntity {
 
   @Column()
   password: string;
+
+  @OneToMany((type) => Board, (board) => board.user, { eager: true })
+  boards: Board[];
 }
